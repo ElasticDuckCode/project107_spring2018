b8bb1b03a8a51453bf74f1e80ba07f912c0de7e9
ElasticDuckCode <jmmillhiser@gmail.com>
2018-05-27 16:38:53 -0700

    turned user input into its own function

--- capacitor.py
+++ capacitor.py
@@ -1,12 +1,24 @@
 #!/usr/bin/env python3
+import math
+import numpy as np
+
+def getNumberOfSquares():
+    squareNumberN = int(input("How many squares to descretize into (give n in 4^n): "))
+    if squareNumberN <= 0:
+        print("Can't do such a calculation.")
+        return 0
+    else:
+        return  4**squareNumberN
 
 def main():
     w = 1e-2  # width of capacitor
     d = 3e-3  # distance between capacitor plates
 
-    squareNumberN = int(input("How many squares to descretize into (give n in 4^n): "))
-    squareNumber = 4**squareNumberN
-    print(squareNumber)  # FOR DEGBUGGING
+    squareNumber = getNumberOfSquares(); print(squareNumber, " -> number of squares")  # FOR DEGBUGGING
+
+    dx = ((w**2) / squareNumber)**(1/2); print(dx, " -> dx value")  # FOR DEGBUGGING
+    return 0
+
 
 main()
 
