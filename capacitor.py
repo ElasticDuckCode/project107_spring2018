7f6bfc41d02a6921e79d1ef974bee6b8afbae03b
fruitsnackies <lkyi@ucsd.edu>
2018-05-27 16:35:54 -0700

    added capacitor creation function

--- capacitor.py
+++ capacitor.py
@@ -3,10 +3,19 @@
 def main():
     w = 1e-2  # width of capacitor
     d = 3e-3  # distance between capacitor plates
+    capCoord = []
 
     squareNumberN = int(input("How many squares to descretize into (give n in 4^n): "))
     squareNumber = 4**squareNumberN
-    print(squareNumber)  # FOR DEGBUGGING
+    print(squareNumber)  # FOR DEGBUGGING 
 
+#creating array of 0's for later use
+def createCap(capCoord): 
+	for i in range(0,squareNumber):
+		capCoord.append(0 0 0)
+
+def calcDist(capCoord):
+	for i in range(0,squareNumber):
+		capCoord[]
 main()
 
