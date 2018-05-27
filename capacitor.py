586cb8ae8e6d0e772854b37fc0961572923def65
ElasticDuckCode <jmmillhiser@gmail.com>
2018-05-27 15:21:21 -0700

    Now gets user input for ammount of squares to divide capacitor into

--- capacitor.py
+++ capacitor.py
@@ -4,5 +4,9 @@ def main():
     w = 1e-2  # width of capacitor
     d = 3e-3  # distance between capacitor plates
 
+    squareNumberN = int(input("How many squares to descretize into (give n in 4^n): "))
+    squareNumber = 4**squareNumberN
+    print(squareNumber)
+
 main()
 
