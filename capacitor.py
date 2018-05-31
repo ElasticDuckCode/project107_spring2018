3753f8605a0769d433859c563ff2792ac2410379
ElasticDuckCode <jmmillhiser@gmail.com>
2018-05-31 13:22:06 -0700

    fixed capacitanceVtime graph

--- capacitor.py
+++ capacitor.py
@@ -12,22 +12,23 @@ def main():
 
     printTitle() # print our cool title
     squareNumber = getNumberOfSquares()  # Obtain the number of squares wanted from the user
+
     # Problem 1
     solveDiscrete(w, d0, v0, 1, squareNumber)
 
     # Problem 2
-#    d1 = 1e-3
-#    d2 = 4e-2
-#    step = d1
-#    current = d1
-#    c = []
-#
-#    while current <= d2:
-#        c.append(solveDiscrete(w, current, v0, 0, squareNumber))
-#        current += step
-#        print("Done! on step: ", current)
-#    c = np.array(c)
-#
-#    discreteVSFormula(c, d1, d2, step, w)
+    d1 = 1e-3
+    d2 = 4e-2
+    step = d1
+    current = d1
+    c = []
+
+    while current <= d2:
+        c.append(solveDiscrete(w, current, v0, 0, squareNumber))
+        current += step
+        print("Done! on step: ", current)
+    c = np.array(c)
+
+    discreteVSFormula(c, d1, d2, step, w)
 
 main()
